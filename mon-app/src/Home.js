808f239108955367a5d4471076e687080c3cc5a2
Meir Belaich <m.Belaich@nerae.local>
2022-01-12 13:30:22 +0100

    using state

--- mon-app/src/Home.js
+++ mon-app/src/Home.js
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
+
+
 const Home = () => {
 
+ //let name ="meir";
+ const [name, setName] = useState("meir");
+ const [age, setAge] = useState(23);
+
+
  const handleClick = (e) => {
-  console.log('ninja', e)
+  setName('belo')
+  setAge(30)
+  //name = 'belo';
+  //console.log('ninja', e)
  }
 
  const handleClick2 = (name, e) => {
@@ -10,6 +22,7 @@ const Home = () => {
  return ( 
   <div className="home">
        <h2>home page</h2>
+       <p>{name} is {age} old</p>
        <button onClick={handleClick}>Click me</button>
        <button onClick = { (e) => handleClick2('meir', e)}> Click me 2 </button>
   </div>
