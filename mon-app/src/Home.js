b55a92d98c64c2040b417d07d79cd72b37e61ab4
Meir Belaich <m.Belaich@nerae.local>
2022-01-12 12:33:30 +0100

    Click event

--- mon-app/src/Home.js
+++ mon-app/src/Home.js
@@ -1,9 +1,17 @@
 const Home = () => {
+
+ const handleClick = (e) => {
+  console.log('ninja', e)
+ }
+
+ const handleClick2 = (name, e) => {
+  console.log('hello' + name, e.target)
+ }
  return ( 
   <div className="home">
-    <h1>
        <h2>home page</h2>
-    </h1>
+       <button onClick={handleClick}>Click me</button>
+       <button onClick = { (e) => handleClick2('meir', e)}> Click me 2 </button>
   </div>
   );
 }
