7fdeaf07f5ccd9f9891ca7ead75a50a9d20a221b
Meir Belaich <m.Belaich@nerae.local>
2022-01-12 14:08:25 +0100

    ouputting Lists

--- mon-app/src/Home.js
+++ mon-app/src/Home.js
@@ -19,12 +19,29 @@ const Home = () => {
  const handleClick2 = (name, e) => {
   console.log('hello' + name, e.target)
  }
+
+
+ //vdeo 10
+ const [blogs, setBlogs] = useState([
+  { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+  { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+  { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+ ])
  return ( 
   <div className="home">
        <h2>home page</h2>
        <p>{name} is {age} old</p>
        <button onClick={handleClick}>Click me</button>
        <button onClick = { (e) => handleClick2('meir', e)}> Click me 2 </button>
+
+       {blogs.map((blog) => (
+       <div className="blog-preview" key={blog.id}>
+            <h2>ecrit par{blog.title}</h2>
+            <p> ecrit par {blog.author}</p>
+
+       </div>
+       ))}
+
   </div>
   );
 }
