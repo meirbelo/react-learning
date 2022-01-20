b88abcb894377a916de6b46a5e75de1ad0c0b0f2
Meir Belaich <m.Belaich@nerae.local>
2022-01-20 10:46:06 +0100

    props #11

--- mon-app/src/Home.js
+++ mon-app/src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import BlogList from "./BlogList";
 
 
 
@@ -33,15 +34,7 @@ const Home = () => {
        <p>{name} is {age} old</p>
        <button onClick={handleClick}>Click me</button>
        <button onClick = { (e) => handleClick2('meir', e)}> Click me 2 </button>
-
-       {blogs.map((blog) => (
-       <div className="blog-preview" key={blog.id}>
-            <h2>ecrit par{blog.title}</h2>
-            <p> ecrit par {blog.author}</p>
-
-       </div>
-       ))}
-
+       <BlogList  blogs={blogs} title='all blog'/>
   </div>
   );
 }
