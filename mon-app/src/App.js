f5cd88a9c73915eb120cc3d1816565d2b6a8d64b
Meir Belaich <m.Belaich@nerae.local>
2022-01-26 14:10:55 +0100

    commit all

--- mon-app/src/App.js
+++ mon-app/src/App.js
@@ -1,7 +1,9 @@
-import './App.css';
-import React from 'react';
+import React , { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 import Home from './Home';
+import Debounce from './Debounce';
+import AutoComplete from './AutoComplete';
+import Split from './Split';
 function App() {
    const title = "welcome to new blog";
   const likes = 50;
@@ -11,20 +13,9 @@ function App() {
     <div className="App">
        <Navbar/>
        <Home/>
-      {/* video 4 */}
-      {/* <div className='content4'>
-        <h1>{title}</h1>
-        <p> nombre de likes :{likes}</p>
-        <p> nombre de likes{person.age}</p>
-        <p> nombre de likes{person.name}</p>
-        <p>{10}</p>
-        <p>{'hi meir'}</p>
-        <p>{[1,2,3,4,5]}</p>
-        <p>{Math.random() * 10}</p>
-
-
-        <a href={link}>Googe site </a>
-      </div> */}
+       <Debounce/>
+       <AutoComplete/>
+       <Split />
     </div>
   );
 }
