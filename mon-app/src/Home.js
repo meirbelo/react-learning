9c280b5b21682bbebed7b9628057ecc8dd1a72c3
meirbelo <meir1.belaich@epitech.eu>
2022-03-02 12:07:14 +0100

    #18

--- mon-app/src/Home.js
+++ mon-app/src/Home.js
@@ -8,6 +8,7 @@ const Home = () => {
  //let name ="meir";
 //  const [name, setName] = useState("meir");
  const [age, setAge] = useState(23);
+ const [isPending, setIsPending] = useState(true);
 
 
  const handleClick = (e) => {
@@ -25,15 +26,18 @@ const Home = () => {
  //vdeo 10
  const [blogs, setBlogs] = useState(null)
  const [name, setName] = useState('Mario')
-
+ 
  useEffect(() => {
-  fetch('http://localhost:8000/blogs')
+  setTimeout(()=> {
+    fetch('http://localhost:8000/blogs')
   .then(res => {
     return res.json();
   })
   .then(data=> {
   setBlogs(data)
+  setIsPending(false)
   })
+  }, 1000)
  },[])
 
  return ( 
@@ -43,7 +47,8 @@ const Home = () => {
        <p>{name} is {age} old</p>
        <button onClick={handleClick}>Click me</button>
        <button onClick = { (e) => handleClick2('meir', e)}> Click me 2 </button>
-    {blogs && <BlogList  blogs={blogs} title='all blog'/>}
+       {isPending && <div>loading...</div>}
+       {blogs && <BlogList  blogs={blogs} title='all blog'/>}
        <button onClick={()=> setName('guili')}>change name to luigi</button>
        <p>{name}</p>
   </div>
