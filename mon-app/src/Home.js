00d71500f5b4f7f9d6f0796b0deaa8eaa96be223
meirbelo <meir1.belaich@epitech.eu>
2022-03-02 12:26:18 +0100

    19

--- mon-app/src/Home.js
+++ mon-app/src/Home.js
@@ -9,7 +9,7 @@ const Home = () => {
 //  const [name, setName] = useState("meir");
  const [age, setAge] = useState(23);
  const [isPending, setIsPending] = useState(true);
-
+ const [error, setError] = useState(null)
 
  const handleClick = (e) => {
 //  setName('belo')
@@ -30,13 +30,22 @@ const Home = () => {
  useEffect(() => {
   setTimeout(()=> {
     fetch('http://localhost:8000/blogs')
-  .then(res => {
-    return res.json();
-  })
-  .then(data=> {
-  setBlogs(data)
-  setIsPending(false)
-  })
+    .then(res => {
+      console.log(res)
+     if(!res.ok) {
+       throw Error('couldnot fetych the datat for that ressource')
+     }
+      return res.json();
+    })
+    .then(data=> {
+    setBlogs(data)
+    setIsPending(false)
+    setError(null);
+    })
+    .catch(err => {
+      setIsPending(false)
+        setError(err.message)
+    })
   }, 1000)
  },[])
 
@@ -47,6 +56,7 @@ const Home = () => {
        <p>{name} is {age} old</p>
        <button onClick={handleClick}>Click me</button>
        <button onClick = { (e) => handleClick2('meir', e)}> Click me 2 </button>
+       {error && <div>{error}</div>}
        {isPending && <div>loading...</div>}
        {blogs && <BlogList  blogs={blogs} title='all blog'/>}
        <button onClick={()=> setName('guili')}>change name to luigi</button>
